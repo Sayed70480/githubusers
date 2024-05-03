b12b53e903e80961cc0e8ae3bb295aec1799ac67
Syed sikandar Shah <88287750+Sayed70480@users.noreply.github.com>
2024-05-03 14:29:32 +0500

    files added

---
+++ dist/index.js
@@ -0,0 +1,37 @@
+"use strict";
+const userName = document.querySelector("#user");
+const formSubmit = document.querySelector("#form");
+const main_container = document.querySelector(".main_container");
+async function myCustomFetcher(url, options) {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+        throw new Error(`Network Response was not ok - status : ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+}
+function showResultUI(singleUser) {
+    const { location, login, avatar_url, url } = singleUser;
+    main_container.insertAdjacentHTML("beforeend", `
+      <div class="card">
+        <img src=${avatar_url} alt=${login}/> <!-- Inserting user avatar image -->
+        <hr/> <!-- Horizontal line -->
+        <div>
+          <img src="${avatar_url}" alt="${login}"/> <!-- Inserting user avatar image again -->
+          <a href="${url}"> GitHub </a> <!-- Link to user's GitHub profile -->
+        </div>
+      </div>
+      `);
+}
+async function fetchUserData(url) {
+    try {
+        const userinfo = await myCustomFetcher(url, {});
+        for (const singleUser of userinfo) {
+            showResultUI(singleUser);
+        }
+    }
+    catch (error) {
+        console.error("Error fetching user data:", error);
+    }
+}
+fetchUserData("https://api.github.com/users");
